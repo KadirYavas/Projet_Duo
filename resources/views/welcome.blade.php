ecf60c7765f77591470958f96108e52fb553776d
Thoms-code <catalanothomas@outlook.fr>
2020-03-12 09:45:20 +0100

    modification blade 'welcome'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,94 +1,104 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <title>Laravel</title>
+    <title>Accueil | Librairie Exo</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #f8f9fa;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        /*.full-height {*/
+        /*    height: 100vh;*/
+        /*}*/
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .links > a {
+            color: #eb412f;
+            padding: 0 25px;
+            font-size: 23px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="{{ asset('image/laravelLogo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
+        Librairie Exo
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('homepage') }}">Accueil<span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Administration
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Utilisateurs</a>
+                    <a class="dropdown-item" href="#">Livres</a>
+                    <a class="dropdown-item" href="#">Auteurs</a>
+                    <a class="dropdown-item" href="#">Genres</a>
                 </div>
-            @endif
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="flex-center position-ref full-height">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Projet Duo
-                </div>
 
-                <div class="links">
-                    <a href="{{route('ajoutAvatar')}}">Avatar</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                </div>
-            </div>
+    <div class="content">
+        <div class="title mt-5 m-b-md w-100">
+            <img  src="{{ asset('image/1200px-Laravel.svg.png') }}" width="400">
         </div>
-    </body>
+        <img class="mb-5" src="{{ asset('image/laravel-wordmark-1.svg') }}" width="600">
+
+        <div class="links mt-4">
+            <a href="{{ route('ajoutAvatar') }}">AVATAR</a>
+            <a href="#">HOMEPAGE</a>
+        </div>
+    </div>
+</div>
+{{--    scripts--}}
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
 </html>
